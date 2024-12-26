fd60c14cad4df70db42562f783a9e33d09a855bb
Linad <merzlikindaniil97@gmail.com>
2024-12-26 17:55:15 +0300

    Добавлен поиск совпадений

--- app/src/main/java/com/example/androidlazygrid/MainActivity.kt
+++ app/src/main/java/com/example/androidlazygrid/MainActivity.kt
@@ -10,8 +10,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -24,8 +22,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.androidlazygrid.ui.theme.AndroidLazyGridTheme
-import kotlin.contracts.contract
+import multiplatform.network.cmptoast.ToastDuration
+import multiplatform.network.cmptoast.showToast
 
 val images = listOf(
     R.drawable.banana,
@@ -59,6 +57,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    val arrayArrays = arrayListOf<Int>()
+    val array = arrayListOf<Int>()
+    var buf = 0
+    var flag = true
+    var count = 0
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
         modifier = Modifier
@@ -67,19 +70,41 @@ fun GreetingPreview() {
         horizontalArrangement = Arrangement.Center
     ) {
         items(100) {
+            buf = images.random()
+            array.add(buf)
             Column(
                 modifier = Modifier
                     .size(120.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Image(
-                    painterResource(id = images.random()),
+                    painterResource(id = buf),
                     contentDescription = "",
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
                         .border(2.dp, Color.Green)
                         .fillMaxSize()
                 )
+            }
+            if (array.size == 3) {
+                array.forEach {
+                    if (it != buf) {
+                        flag = false
+                    }
+                }
+                if (flag) {
+                    showToast(
+                        message = "Поздравляем, найдено совпадение",
+                        backgroundColor = Color.White,
+                        textColor = Color.Black,
+                        duration = ToastDuration.Short
+                    )
+                }
+                array.forEach {
+                    arrayArrays.add(it)
+                }
+                array.clear()
+                flag = true
             }
         }
     }
